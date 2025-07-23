83595937bc9fa837a41f468308982bec4fc1fd39
Anggit Septiansyah <anggitseptiansyah@gmail.com>
2025-07-23 10:27:12 +0700

    adding media query in sidebar

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,9 +1,16 @@
-const Header = () => {
+import PropTypes from "prop-types"
+
+const Header = ({collapsed, setCollapsed}) => {
   return (
-    <header>
+    <header className="relative z-10 flex h-[60px]">
         Header
     </header>
   )
+}
+
+Header.PropTypes = {
+  collapsed: PropTypes.bool,
+  setCollapsed: PropTypes.func,
 }
 
 export default Header
